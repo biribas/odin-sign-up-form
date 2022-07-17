11c39ecdfb0ab774ea1853ff8ed1a429e30d6907
Carlos Adriano <cadrianogamac@gmail.com>
2022-07-17 15:51:09 -0300

    Create validate form function

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const passwordInputs = document.querySelectorAll('#password-inputs input');
 const password = passwordInputs[0];
 const confirmPassowrd = passwordInputs[1];
 
-inputs.forEach(input => input.addEventListener('focusout', () => {
-  if (!input.checkValidity())
-    input.classList.add('invalid');
-}));
-
-inputs.forEach(input => input.addEventListener('input', () => {
-  if (input.checkValidity())
-    input.classList.remove('invalid');
-}));
+function validateForm(event) {
+  inputs.forEach(input => {
+    if (!input.checkValidity()) {
+      input.classList.add('invalid');
+      event.preventDefault();
+    }
+  });
+  document.querySelector('.invalid').focus();
+}
 
 function handlePasswordFocusOut() {
   if (this.checkValidity() && password.checkValidity() && this.value !== password.value) 
@@ -28,5 +28,17 @@ function handlePasswordInput() {
     confirmPassowrd.classList.remove('match');
 }
 
+inputs.forEach(input => input.addEventListener('focusout', () => {
+  if (!input.checkValidity())
+    input.classList.add('invalid');
+}));
+
+inputs.forEach(input => input.addEventListener('input', () => {
+  if (input.checkValidity())
+    input.classList.remove('invalid');
+}));
+
 passwordInputs.forEach(input => input.addEventListener('input', handlePasswordInput));
 confirmPassowrd.addEventListener('focusout', handlePasswordFocusOut);
+
+document.addEventListener('submit', validateForm);
