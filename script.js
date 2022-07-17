1815dd24bd552e2c3280f56d6588ad734cb63fa1
Carlos Adriano <cadrianogamac@gmail.com>
2022-07-17 14:41:24 -0300

    Add validation logic

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const inputs = document.querySelectorAll('input');
+const passwordInputs = document.querySelectorAll('#password-inputs input');
+const password = passwordInputs[0];
+const confirmPassowrd = passwordInputs[1];
 
-inputs.forEach(input => input.addEventListener('focusout', e => {
+inputs.forEach(input => input.addEventListener('focusout', () => {
   if (!input.checkValidity())
     input.classList.add('invalid');
-  else
+}));
+
+inputs.forEach(input => input.addEventListener('input', () => {
+  if (input.checkValidity())
     input.classList.remove('invalid');
 }));
+
+function handlePasswordFocusOut() {
+  if (this.checkValidity() && password.checkValidity() && this.value !== password.value) 
+    this.classList.add('invalid');
+}
+
+function handlePasswordInput() {
+  if ((this.id === 'password' && this.checkValidity() && this.value === confirmPassowrd.value) || 
+    (this.id === 'confirm-password' && this.checkValidity() && password.checkValidity() && this.value === password.value)) {
+    confirmPassowrd.classList.remove('invalid');
+    confirmPassowrd.classList.add('match');
+  }
+  else 
+    confirmPassowrd.classList.remove('match');
+}
+
+passwordInputs.forEach(input => input.addEventListener('input', handlePasswordInput));
+confirmPassowrd.addEventListener('focusout', handlePasswordFocusOut);
